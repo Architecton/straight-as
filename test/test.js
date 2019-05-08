410a5d32011fe395c39c2d20987b9b00fd342db8
JureTheMan77 <jv9074@student.uni-lj.si>
2019-05-08 11:42:07 +0200

    Narejen preprost test, .gitignore prestavljen 1 nivo višje

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var zahteva = require('../src/node_modules/supertest');
+var streznik = require('../src/streznik.js');
+
+
+describe('Začetna stran', function() {
+    it ('Naslov strani: "Bogdej..."', function(done) {
+        zahteva(streznik).get('/').expect(/<h1>Bogdej, Janez<\/h1>/i, done);
+    });
+
+    it ('Odstavek "brskalnik"', function(done) {
+        zahteva(streznik).get('/').expect(/<p>Mislš da jst ne vem, da si se prpelu z <\/p>/i, done);
+    });
+});
