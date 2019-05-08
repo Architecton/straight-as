98e397356dbe8d91d62f7bbb0ab57a06a3fdc297
JureTheMan77 <jv9074@student.uni-lj.si>
2019-05-08 09:55:48 +0200

    dummy projekt + gitignore

---
+++ src/streznik.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var streznik = express();
+var port = process.env.PORT || 3000;
+
+
+/**
+ * Ko uporabnik obišče začetno stran,
+ * izpiši začetni pozdrav
+ */
+streznik.get('/', function (zahteva, odgovor) {
+    odgovor.send(
+        '<h1>Bogdej, Janez</h1>' +
+        '<p>Mislš da jst ne vem, da si se prpelu z </p>' +
+        '<pre>' + zahteva.get('User-Agent') + '</pre>'
+    );
+});
+
+
+/**
+ * Poženi strežnik
+ */
+streznik.listen(port, function () {
+    console.log('Strežnik je pognan na odprtini ' + port + ' ajga.');
+});
+
+
+module.exports = streznik;
